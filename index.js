752a56525a49c810367810ffad4f7b78d60405c7
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-11 15:07:03 +0200

    js udelane a trichu dostylovane

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('funguju');
+
+const kosile = document.querySelector('#product-image')
+
+
+
+document.querySelector('#bila').addEventListener('click', () => {
+  kosile.style = "fill: white"
+})  
+
+document.querySelector('#cerna').addEventListener('click', () => {
+  kosile.style = "fill: black"
+})
+
+document.querySelector('#cervena').addEventListener('click', () => {
+  kosile.style = "fill: firebrick"
+})
+
+document.querySelector('#modra').addEventListener('click', () => {
+  kosile.style = "fill: skyblue"
+})
